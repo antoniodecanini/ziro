624429a03e743de8ae3476db0ba4b69477cd4f98
Antonio Decanini <antonio.decanini@hotmail.com>
2020-08-28 10:26:50 -0300

    Frontend: Create Routes

---
+++ web/src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Landing from './pages/Landing';
+import Cart from './pages/Cart';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Landing} />
+      <Route path="/cart" component={Cart} />
+    </BrowserRouter>
+  );
+}
