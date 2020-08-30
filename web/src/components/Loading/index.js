773ff018eb9be9fa108141bc627e30cdc3e8b093
Antonio Decanini <antonio.decanini@hotmail.com>
2020-08-30 16:03:36 -0300

    Frontend: Create Loading Component

---
+++ web/src/components/Loading/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function Loading() {
+  return (
+    <div className="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
