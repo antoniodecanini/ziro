8e8123ea1c9b9c48770833d6b88e8a08d286bdcc
Antonio Decanini <antonio.decanini@hotmail.com>
2020-09-01 21:46:19 -0300

    Backend: Create indexProducts Function

---
+++ backend/functions/indexProducts.js
@@ -0,0 +1,22 @@
+const Product = require("../src/Models/Product");
+const headers = require("../src/config/headers");
+
+exports.handler = async function (event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  try {
+    const products = await Product.find({});
+
+    callback(null, {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ products }),
+    });
+  } catch (err) {
+    callback(null, {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify("Search products error:", err),
+    });
+  }
+};
